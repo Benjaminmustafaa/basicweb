64a306c57610e2794a3bb36010ba9295e6c676f3
Benjamin Mustafaa <beni93m_@hotmail.com>
2021-11-09 17:15:34 +0100

    add users into table view

--- src/main/java/com/benxhi/basicweb/controller/FitnessController.java
+++ src/main/java/com/benxhi/basicweb/controller/FitnessController.java
@@ -1,13 +1,29 @@
 package com.benxhi.basicweb.controller;
-
-
+import com.benxhi.basicweb.model.FitnessModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.ArrayList;
 
 @Controller
 public class FitnessController {
-    @GetMapping("/Fitness")
-    public String getFitness(){
-        return "FitnessApp";
+
+    ArrayList<FitnessModel> fitnessUsers = new ArrayList<>();
+    @GetMapping("/addfitnessusers")
+    public String getFitness(Model model) {
+        model.addAttribute("fitnessUserModel", new FitnessModel());
+        return "AddUsers";
+    }
+    @PostMapping("/viewfitnessusers")
+    public String createUser(@ModelAttribute FitnessModel fitnessModel, Model model) {
+        fitnessUsers.add(fitnessModel);
+        model.addAttribute("fitnessUsers", fitnessUsers);
+        return "ViewUsers";
+    }
+    @GetMapping("/viewfitnessusers")
+    public String getViewUsers(){
+        return "ViewUsers";
     }
 }
--- src/main/java/com/benxhi/basicweb/model/FitnessModel.java
+++ src/main/java/com/benxhi/basicweb/model/FitnessModel.java
@@ -1,52 +1,49 @@
 package com.benxhi.basicweb.model;
-
 public class FitnessModel {
 
-        private String name;
-        private String lastName;
-        private int age;
-        private String gjinia;
-        private boolean isActive;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        public String getGjinia() {
-            return gjinia;
-        }
-
-        public void setGjinia(String gjinia) {
-            this.gjinia = gjinia;
-        }
-
-        public boolean isActive() {
-            return isActive;
-        }
-
-        public void setActive(boolean active) {
-            isActive = active;
-        }
-
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender;
+    private boolean isActive;
+    public String getFirstName() {
+        return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+}
